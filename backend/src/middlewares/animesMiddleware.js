8323a613d140eeaefc2097e2f2746f2005819e1f
Rian Santos <85030276+ryanms2@users.noreply.github.com>
2024-02-02 14:17:08 -0300

    Update animesMiddleware.js

--- backend/src/middlewares/animesMiddleware.js
+++ backend/src/middlewares/animesMiddleware.js
@@ -12,21 +12,6 @@ const validaTitulo = (req, res, next) => {
     next();
 }
 
-const validaImagem = (req, res, next) => {
-    const { body } = req;
-
-    if (body.imagem == undefined) {
-        return res.status(400).json({message: 'A imagem é necessária.'});
-    };
-
-    if (body.imagem == '') {
-        return res.status(400).json({message: 'A imagem não pode estar vazia.'});
-    };
-
-    next();
-};
-
 module.exports = {
-    validaTitulo,
-    validaImagem
+    validaTitulo
 };
